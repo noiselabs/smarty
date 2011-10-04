noiselabs/smarty

commit 056ae5df7a084eccc45129ff0f6b3b4791d97cd3

- improvement replaced most in_array() calls by more efficient isset() on array_flip()ed haystacks - added notes on possible performance optimization/problem with Smarty_Security
git-svn-id: http://smarty-php.googlecode.com/svn/trunk@4342 9dce5a81-9a42-0410-99e4-3799d3902e7f

distribution/libs/plugins/function.math.php
@@ -23,6 +23,11 @@
  */
 function smarty_function_math($params, $template)
 {
+    static $_allowed_funcs = array(
+        'int' => true, 'abs' => true, 'ceil' => true, 'cos' => true, 'exp' => true, 'floor' => true, 
+        'log' => true, 'log10' => true, 'max' => true, 'min' => true, 'pi' => true, 'pow' => true, 
+        'rand' => true, 'round' => true, 'sin' => true, 'sqrt' => true, 'srand' => true ,'tan' => true
+    );
     // be sure equation parameter is present
     if (empty($params['equation'])) {
         trigger_error("math: missing equation parameter",E_USER_WARNING);
@@ -39,11 +44,9 @@ function smarty_function_math($params, $template)
 
     // match all vars in equation, make sure all are passed
     preg_match_all("!(?:0x[a-fA-F0-9]+)|([a-zA-Z][a-zA-Z0-9_]*)!",$equation, $match);
-    $allowed_funcs = array('int','abs','ceil','cos','exp','floor','log','log10',
-                           'max','min','pi','pow','rand','round','sin','sqrt','srand','tan');
 
     foreach($match[1] as $curr_var) {
-        if ($curr_var && !in_array($curr_var, array_keys($params)) && !in_array($curr_var, $allowed_funcs)) {
+        if ($curr_var && !isset($params[$curr_var]) && !isset($_allowed_funcs[$current_var])) {
             trigger_error("math: function call $curr_var not allowed",E_USER_WARNING);
             return;
         }
@@ -52,7 +55,7 @@ function smarty_function_math($params, $template)
     foreach($params as $key => $val) {
         if ($key != "equation" && $key != "format" && $key != "assign") {
             // make sure value is not empty
-            if (strlen($val)==0) {
+            if (isset($val[0])) {
                 trigger_error("math: parameter $key is empty",E_USER_WARNING);
                 return;
             }

distribution/libs/plugins/function.cycle.php
@@ -52,7 +52,7 @@ function smarty_function_cycle($params, $template)
     $advance = (isset($params['advance'])) ? (bool)$params['advance'] : true;
     $reset = (isset($params['reset'])) ? (bool)$params['reset'] : false;
 
-    if (!in_array('values', array_keys($params))) {
+    if (!isset($params['values'])) {
         if(!isset($cycle_vars[$name]['values'])) {
             trigger_error("cycle: missing 'values' parameter");
             return;

distribution/libs/plugins/function.html_options.php
@@ -66,7 +66,7 @@ function smarty_function_html_options($params, $template)
 
             case 'selected':
                 if (is_array($_val)) {
-                    $selected = array_map('strval', array_values((array)$_val));
+                    $selected = array_flip(array_map('strval', array_values((array)$_val)));
                 } else {
                     $selected = $_val;
                 }
@@ -114,7 +114,7 @@ function smarty_function_html_options_optoutput($key, $value, $selected, $id, $c
     if (!is_array($value)) {
         $_html_result = '<option value="' . smarty_function_escape_special_chars($key) . '"';
         if (is_array($selected)) {
-            if (in_array((string)$key, $selected)) {
+            if (isset($selected[(string) $key])) {
                 $_html_result .= ' selected="selected"';
             }
         } elseif ($key == $selected) {

distribution/libs/plugins/function.mailto.php
@@ -50,6 +50,7 @@
  */
 function smarty_function_mailto($params, $template)
 {
+    static $_allowed_encoding = array('javascript' => true, 'javascript_charcode' => true, 'hex' => true, 'none' => true);
     $extra = '';
 
     if (empty($params['address'])) {
@@ -95,7 +96,7 @@ function smarty_function_mailto($params, $template)
     $address .= $mail_parm_vals;
 
     $encode = (empty($params['encode'])) ? 'none' : $params['encode'];
-    if (!in_array($encode, array('javascript', 'javascript_charcode', 'hex', 'none'))) {
+    if (!isset($_allowed_encoding[$encode])) {
         trigger_error("mailto: 'encode' parameter must be none, javascript, javascript_charcode or hex", E_USER_WARNING);
         return;
     }

distribution/libs/sysplugins/smarty_internal_compile_break.php
@@ -41,6 +41,7 @@ class Smarty_Internal_Compile_Break extends Smarty_Internal_CompileBase {
      */
     public function compile($args, $compiler, $parameter)
     {
+        static $_is_loopy = array('for' => true, 'foreach' => true, 'while' => true, 'section' => true);
         // check and get attributes
         $_attr = $this->getAttributes($compiler, $args);
 
@@ -59,7 +60,7 @@ class Smarty_Internal_Compile_Break extends Smarty_Internal_CompileBase {
         $level_count = $_levels;
         $stack_count = count($compiler->_tag_stack) - 1;
         while ($level_count > 0 && $stack_count >= 0) {
-            if (in_array($compiler->_tag_stack[$stack_count][0], array('for', 'foreach', 'while', 'section'))) {
+            if (isset($_is_loopy[$compiler->_tag_stack[$stack_count][0]])) {
                 $level_count--;
             }
             $stack_count--;

distribution/libs/sysplugins/smarty_internal_compile_config_load.php
@@ -48,6 +48,7 @@ class Smarty_Internal_Compile_Config_Load extends Smarty_Internal_CompileBase {
      */
     public function compile($args, $compiler)
     {
+        static $_is_legal_scope = array('local' => true,'parent' => true,'root' => true,'global' => true);
         // check and get attributes
         $_attr = $this->getAttributes($compiler, $args);
 
@@ -67,7 +68,7 @@ class Smarty_Internal_Compile_Config_Load extends Smarty_Internal_CompileBase {
         // scope setup
         if (isset($_attr['scope'])) {
             $_attr['scope'] = trim($_attr['scope'], "'\"");
-            if (in_array($_attr['scope'],array('local','parent','root','global'))) {
+            if (isset($_is_legal_scope[$_attr['scope']])) {
                 $scope = $_attr['scope'];
            } else {
                 $compiler->trigger_template_error('illegal value for "scope" attribute', $compiler->lex->taglineno);

distribution/libs/sysplugins/smarty_internal_compile_continue.php
@@ -42,6 +42,7 @@ class Smarty_Internal_Compile_Continue extends Smarty_Internal_CompileBase {
      */
     public function compile($args, $compiler, $parameter)
     {
+        static $_is_loopy = array('for' => true, 'foreach' => true, 'while' => true, 'section' => true);
         // check and get attributes
         $_attr = $this->getAttributes($compiler, $args);
 
@@ -60,7 +61,7 @@ class Smarty_Internal_Compile_Continue extends Smarty_Internal_CompileBase {
         $level_count = $_levels;
         $stack_count = count($compiler->_tag_stack) - 1;
         while ($level_count > 0 && $stack_count >= 0) {
-            if (in_array($compiler->_tag_stack[$stack_count][0], array('for', 'foreach', 'while', 'section'))) {
+            if (isset($_is_loopy[$compiler->_tag_stack[$stack_count][0]])) {
                 $level_count--;
             }
             $stack_count--;

distribution/libs/sysplugins/smarty_internal_compile_extends.php
@@ -42,6 +42,7 @@ class Smarty_Internal_Compile_Extends extends Smarty_Internal_CompileBase {
     */
     public function compile($args, $compiler)
     {
+        static $_is_stringy = array('string' => true, 'eval' => true);
         $this->_rdl = preg_quote($compiler->smarty->right_delimiter);
         $this->_ldl = preg_quote($compiler->smarty->left_delimiter);
         $filepath = $compiler->template->source->filepath;
@@ -60,7 +61,7 @@ class Smarty_Internal_Compile_Extends extends Smarty_Internal_CompileBase {
         // create template object
         $_template = new $compiler->smarty->template_class($include_file, $compiler->smarty, $compiler->template);
         // save file dependency
-        if (in_array($_template->source->type, array('eval', 'string'))) {
+        if (isset($_is_stringy[$_template->source->type])) {
             $template_sha1 = sha1($include_file);
         } else {
             $template_sha1 = sha1($_template->source->filepath);

commit 289c437d9dbe3500f9a7597a577d3351aca17043

- bugfix of problem introduced with r4342 on math plugin

distribution/libs/plugins/function.math.php
@@ -24,8 +24,8 @@
 function smarty_function_math($params, $template)
 {
     static $_allowed_funcs = array(
-        'int' => true, 'abs' => true, 'ceil' => true, 'cos' => true, 'exp' => true, 'floor' => true, 
-        'log' => true, 'log10' => true, 'max' => true, 'min' => true, 'pi' => true, 'pow' => true, 
+        'int' => true, 'abs' => true, 'ceil' => true, 'cos' => true, 'exp' => true, 'floor' => true,
+        'log' => true, 'log10' => true, 'max' => true, 'min' => true, 'pi' => true, 'pow' => true,
         'rand' => true, 'round' => true, 'sin' => true, 'sqrt' => true, 'srand' => true ,'tan' => true
     );
     // be sure equation parameter is present
@@ -55,7 +55,7 @@ function smarty_function_math($params, $template)
     foreach($params as $key => $val) {
         if ($key != "equation" && $key != "format" && $key != "assign") {
             // make sure value is not empty
-            if (isset($val[0])) {
+            if (strlen($val)==0) {
                 trigger_error("math: parameter $key is empty",E_USER_WARNING);
                 return;
             }