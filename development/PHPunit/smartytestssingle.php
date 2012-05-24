d089483b8ad47fffdde566fb62947e8d04a576d0
uwe.tews@googlemail.com <uwe.tews@googlemail.com@9dce5a81-9a42-0410-99e4-3799d3902e7f>
2012-05-24 17:03:31 +0000

    - bugfix add compile_id to cache key of of source (Issue 97)

--- development/PHPunit/smartytestssingle.php
+++ development/PHPunit/smartytestssingle.php
@@ -61,6 +61,8 @@ class SmartyTests extends PHPUnit_Framework_TestSuite {
         $smarty->error_unassigned = true;
         $smarty->caching_type = 'file';
         $smarty->cache_locking = false;
+        $smarty->cache_id = null;
+        $smarty->compile_id = null;
         $smarty->default_resource_type = 'file';
     }
 
